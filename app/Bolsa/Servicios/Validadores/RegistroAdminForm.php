e77bea71fd65e89a2ae813c1b2984aed19206866
@ofauno <omarlopezf@gmail.com>
2014-09-25 18:30:48 -0500

    validation/admin: 8 characters minimum && alpha numeric

--- app/Bolsa/Servicios/Validadores/RegistroAdminForm.php
+++ app/Bolsa/Servicios/Validadores/RegistroAdminForm.php
@@ -13,6 +13,6 @@ class RegistroAdminForm extends FormValidator {
      */
     protected $rules = array(
         'email'          => 'required|email|unique:users',
-        'password'       => 'required|alpha_num|min:6'
+        'password'       => 'required|alpha_num|min:8'
     );
 }
