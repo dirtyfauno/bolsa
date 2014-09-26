c619a793f8a8b8106ccaf92fed63b5bae53abc07
@ofauno <omarlopezf@gmail.com>
2014-09-25 19:58:51 -0500

    view: update to relative path

--- app/views/partials/estilo.blade.php
+++ app/views/partials/estilo.blade.php
@@ -1,4 +1,4 @@
-<link href="http://fonts.googleapis.com/css?family=Muli:400,300,400|Josefin+Slab:100" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="/css/standardize.css">
-<link rel="stylesheet" href="/css/index-grid.css">
-<link rel="stylesheet" href="/css/index.css">
+<link href="//fonts.googleapis.com/css?family=Muli:400,300,400|Josefin+Slab:100" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/standardize.css">
+<link rel="stylesheet" href="css/index-grid.css">
+<link rel="stylesheet" href="css/index.css">
