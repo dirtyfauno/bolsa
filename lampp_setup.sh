ad2d362e9389c03faddf386b00ad02e37994b6be
faunus <faunotv@gmail.com>
2014-09-25 21:32:44 -0500

    rm commas

--- lampp_setup.sh
+++ lampp_setup.sh
@@ -1,12 +1,12 @@
 curl -sS https://getcomposer.org/installer | /opt/lampp/bin/php
 /opt/lampp/bin/php composer.phar dump-autoload --optimize
-/opt/lampp/bin/php artisan key:generate",
-/opt/lampp/bin/php artisan bolsa:setup",
-/opt/lampp/bin/php artisan db:seed --env=bolsa --database=sqlite",
-sudo chmod -R 777 app/storage",
-sudo chmod 777 app/database/production.sqlite",
-sudo chmod 777 app/database/reportes.sqlite",
-/opt/lampp/bin/php composer.phar dump-autoload --optimize",
-/opt/lampp/bin/php artisan optimize",
-sudo chown www-data app/database/production.sqlite",
+/opt/lampp/bin/php artisan key:generate"
+/opt/lampp/bin/php artisan bolsa:setup"
+/opt/lampp/bin/php artisan db:seed --env=bolsa --database=sqlite"
+sudo chmod -R 777 app/storage"
+sudo chmod 777 app/database/production.sqlite"
+sudo chmod 777 app/database/reportes.sqlite"
+/opt/lampp/bin/php composer.phar dump-autoload --optimize"
+/opt/lampp/bin/php artisan optimize"
+sudo chown www-data app/database/production.sqlite"
 sudo chown www-data app/database/reportes.sqlite"
