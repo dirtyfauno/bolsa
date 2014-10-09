690c7fb2aa8a50de31d059444fd67f97f1e7d85b
faunus <faunotv@gmail.com>
2014-10-09 16:12:34 -0500

    quitar usuario actual al actualizar

--- app/Bolsa/Servicios/Validadores/ActualizarEmpresaForm.php
+++ app/Bolsa/Servicios/Validadores/ActualizarEmpresaForm.php
@@ -1,21 +1,33 @@
 <?php namespace Bolsa\Formularios;
 
+use EmpresaModel;
 use Laracasts\Validation\FormValidator;
 
 /**
  * Class ActualizarEmpresaForm
  * @package Bolsa\Formularios
  */
-class ActualizarEmpresaForm extends FormValidator {
-
+class ActualizarEmpresaForm extends FormValidator
+{
     /**
      * @var array
      */
     protected $rules = array(
-        'nombre'     => 'required|unique:empresas,slug',
-        'direccion'  => 'required',
-        'giro'       => 'required',
-        'telefono'   => 'required',
-        'logo'       => 'required|mimes:png,jpg,jpeg|max:1000'
+        'direccion' => 'required',
+        'giro' => 'required',
+        'telefono' => 'required',
+        'logo' => 'required|mimes:png,jpg,jpeg|max:1000'
     );
+
+    /**
+     * @param $id
+     * @return $this
+     */
+    public function excluirUser($id)
+    {
+        $user_id = EmpresaModel::where("user_id", $id)->pluck("id");
+        $this->rules['nombre'] = "required|unique:empresas,slug,$user_id";
+
+        return $this;
+    }
 }
